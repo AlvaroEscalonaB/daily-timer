b1391cba2e3ef7e4dae878e3b3ec48cef8baa54b
Alvaro Escalona <escalona.alvaro.13@gmail.com>
2026-06-01 19:54:04 -0400

    Fix vite env for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/daily-timer/' : '/',
+export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? '/daily-timer/' : '/',
   resolve: { tsconfigPaths: true },
   plugins: [tanstackRouter(), tailwindcss(), viteReact()],
-}))
+})
